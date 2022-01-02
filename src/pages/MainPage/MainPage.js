6113e0447c87ae5950da10260bdc67ffe6cfce62
Roey liba <rliba1990@gmail.com>
2022-01-02 18:44:46 +0200

    just a demo

---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import {
+  AnimationComponent,
+  MainPageContainer,
+  Button,
+  Title,
+  SubTitle,
+} from "./StyledMainPage";
+
+export const MainPage = () => {
+  const [isPrimary, setIsPrimary] = useState(true);
+  return (
+    <MainPageContainer>
+      <Title>My Title</Title>
+      <SubTitle>My SubTitle</SubTitle>
+      <AnimationComponent />
+      <Button
+        primary={isPrimary}
+        onClick={() => {
+          setIsPrimary((prev) => !prev);
+        }}
+      />
+    </MainPageContainer>
+  );
+};
---
+++ src/pages/MainPage/StyledMainPage.js
@@ -0,0 +1,44 @@
+import styled, { css, keyframes } from "styled-components";
+
+const someCss = css`
+  background-color: red;
+`;
+
+export const MainPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  ${someCss}//this will put the background-color: red; here
+`;
+
+export const Button = styled.button.attrs({
+  onmouseover: () => {
+    console.log("onmouseovered");
+  },
+})`
+  background-color: ${({ primary }) => (primary ? "red" : "blue")};
+`;
+
+export const Title = styled.h1`
+`;
+export const SubTitle = styled.h2`
+`;
+
+const animation = keyframes`
+  0% {
+    opacity: 0;
+  }
+
+  100 {
+    opacity: 1;
+  }
+`;
+
+const animationRule = css`
+  ${animation} 1s infinite alternate;
+`;
+
+export const AnimationComponent = styled.div`
+  animation: ${animationRule};
+  height: 100px;
+  width: 100px;
+`;
